c5a0e7ed40dc22bbf880ba95e3a598ea81301aeb
Nishat7155 <nishat_lisa@yahoo.com>
2023-06-02 23:07:23 +0200

    logo created
    app logo

---
+++ movieapp/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="container flexSB">
+      <div className="flexSB">
+        <div className="logo">
+          <img src='./images/logo1.png' alt=""/>
+        </div>
+        <ul className="flexSB"></ul>
+      </div>
+    </div>
+  )
+}
