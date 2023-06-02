78aa269022cf61e12e9b73f7abe11ed0fa47490f
Nishat7155 <nishat_lisa@yahoo.com>
2023-06-03 00:24:36 +0200

    files modified
    index.js,header.jsx,header.css created

--- movieapp/src/components/header/header.jsx
+++ movieapp/src/components/header/header.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import "./header.css";
 
+
 export const Header = () => {
+  const [Mobile, setMobile] = useState(false)
   return (
-    <div className="container flexSB">
-      <div className="flexSB">
-        <div className="logo">
-          <img src='./images/logo1.png' alt=""/>
-        </div>
-        <ul className="flexSB"></ul>
+
+    <header>
+     <div className='container flexSB'>
+       <nav className='flexSB'>
+            <div className='logo'>
+               <img src='./images/logo1.png' alt=""/>
+            </div>
+        <ul className={Mobile ? "navMenu-list" : "flexSB"} onClick={() => setMobile(false)}>
+           <Link to ='/'>Home</Link>
+           <Link to ='/movies'>Movies</Link>
+           <Link to ='/series'>Series</Link>
+           <Link to ='/pages'>Pages</Link>
+           <Link to ='/pricing'>Pricing</Link>
+           <Link to='/contact'>Contact</Link>
+        </ul>
+        <button className="toggle">
+          <i className="fa fa-bars"></i>
+        </button>
+      </nav>
+      <div className="account flexSB">
+            <i className='fa fa-search'></i>
+            <i className='fas fa-bell'></i>
+            <i className='fas fa-user'></i>
+            <button>Subscribe Now</button>
       </div>
-    </div>
+      </div>
+    </header>
   )
 }
+
+export default Header;
